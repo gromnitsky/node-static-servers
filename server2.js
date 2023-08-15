f52fc4fc2dd6799397222b78cc69bfdad6487bab
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2023-08-15 21:47:41 +0300

    server2

--- server2.js
+++ server2.js
@@ -1,15 +1,9 @@
-/*
-  + uses 'mime' from npm
-  - sets last-modified & etag
-  - responds to HEAD requests
-*/
-
 import http from 'http'
 import mime from 'mime'
 import serve_static from './lib/server2.js'
 
 let server = http.createServer( (req, res) => {
-    serve_static(res, req.url, {
+    serve_static(req, res, req.url, {
         headers: { 'access-control-allow-origin': 'example.com' },
         mime: { 'text/markdown': ['txt'] },
         verbose: true
