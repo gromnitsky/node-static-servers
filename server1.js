332674a919210394429fcfbd2366a0dcf678d9fb
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2023-08-15 20:56:42 +0300

    modules

--- server1.js
+++ server1.js
@@ -11,9 +11,8 @@
   - Premature close
 */
 
-import fs from 'fs'
 import http from 'http'
-import path from 'path'
+import serve_static from './lib/server1.js'
 
 let server = http.createServer( (req, res) => {
     serve_static(res, req.url, {
@@ -25,37 +24,3 @@ let server = http.createServer( (req, res) => {
 
 server.listen(process.env.PORT || 3000)
 console.error(process.pid, process.cwd())
-
-function serve_static(writable, name, opt = {}) {
-    if (/^\/+$/.test(name)) name = "index.html"
-    let file = path.join(opt.public_root || process.cwd(), path.normalize(name))
-
-    fs.stat(file, (err, stats) => {
-        if (!err && !stats.isFile()) {
-            err = new Error("Invalid argument")
-            err.code = 'EINVAL'
-        }
-        if (err) return error(writable, err, opt.verbose)
-
-        let readable = fs.createReadStream(file)
-        readable.once('data', () => {
-            writable.setHeader('Content-Length', stats.size)
-            writable.setHeader('Content-Type', Object.assign({
-                '.html': 'text/html',
-                '.js': 'application/javascript'
-            }, opt.mime)[path.extname(file)] || 'application/octet-stream')
-            Object.entries(opt.headers || {}).map(h => writable.setHeader(...h))
-        })
-        readable.on('error', err => error(writable, err, opt.verbose))
-        readable.pipe(writable)
-    })
-}
-
-function error(writable, err, verbose) {
-    if (!writable.headersSent) {
-        let codes = { 'ENOENT': 404, 'EACCES': 403, 'EINVAL': 400 }
-        writable.statusCode = codes[err?.code] || 500
-        if (verbose) writable.statusMessage = err
-    }
-    writable.end()
-}
--- server2.js
+++ server2.js
@@ -4,13 +4,12 @@
   - responds to HEAD requests
 */
 
-import fs from 'fs'
 import http from 'http'
-import path from 'path'
 import mime from 'mime'
+import serve_static from './lib/server2.js'
 
 let server = http.createServer( (req, res) => {
-    serve_static2(res, req.url, {
+    serve_static(res, req.url, {
         headers: { 'access-control-allow-origin': 'example.com' },
         mime: { 'text/markdown': ['txt'] },
         verbose: true
@@ -19,36 +18,3 @@ let server = http.createServer( (req, res) => {
 
 server.listen(process.env.PORT || 3000)
 console.error(process.pid, process.cwd())
-
-function serve_static2(writable, name, opt = {}) {
-    if (/^\/+$/.test(name)) name = "index.html"
-    let file = path.join(opt.public_root || process.cwd(), path.normalize(name))
-
-    fs.stat(file, (err, stats) => {
-        if (!err && !stats.isFile()) {
-            err = new Error("Invalid argument")
-            err.code = 'EINVAL'
-        }
-        if (err) return error(writable, err, opt.verbose)
-
-        let readable = fs.createReadStream(file)
-        readable.once('data', () => {
-            writable.setHeader('Content-Length', stats.size)
-            mime.define(opt.mime, true)
-            writable.setHeader('Content-Type',
-                               mime.getType(file) || 'application/octet-stream')
-            Object.entries(opt.headers || {}).map(h => writable.setHeader(...h))
-        })
-        readable.on('error', err => error(writable, err, opt.verbose))
-        readable.pipe(writable)
-    })
-}
-
-function error(writable, err, verbose) {
-    if (!writable.headersSent) {
-        let codes = { 'ENOENT': 404, 'EACCES': 403, 'EINVAL': 400 }
-        writable.statusCode = codes[err?.code] || 500
-        if (verbose) writable.statusMessage = err
-    }
-    writable.end()
-}
